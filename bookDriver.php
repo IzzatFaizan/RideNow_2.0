848a606c5fc9fad40722cab479c4aa01287f241a
IzzatFaizan <izzatfaiz1996@gmail.com>
2018-05-11 23:58:04 +0800

    mm

--- bookDriver.php
+++ bookDriver.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title> RideNow | Login</title>
+<title>RideNow</title>
 <meta name="description" content="">
 <meta name="author" content="">
 
@@ -18,24 +18,19 @@
 
 </head>
 <body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
-
 <!-- Navigation -->
 <nav id="menu" class="navbar navbar-default navbar-fixed-top">
   <div class="container"> 
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-      <a class="navbar-brand page-scroll" href="index.php"><i class="fa fa-car"></i> RideNow</a> 
-    </div>
+      <a class="navbar-brand page-scroll" href="index.php"><i class="fa fa-car"></i> RideNow</a> </div>
     
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="index.php" class="page-scroll" style="font-size: 15px ;">Home</a></li>
-        <li><a href="index.php" class="page-scroll" style="font-size: 15px ;">About</a></li>
-        <li><a href="index.php" class="page-scroll" style="font-size: 15px ;">Register</a></li>
-        <li><a href="index.php" class="page-scroll" style="font-size: 15px ;">Contact</a></li>
-        <li><a href="login.php" class="page-scroll fa fa-car" style="font-size: 15px ;"> Login</a></li>
+        <li><a href="homepage.php" class="page-scroll" style="font-size: 15px ;">Home</a></li>
+        <li><a href="login.php" class="page-scroll" style="font-size: 15px ;">Login</a></li>
       </ul>
     </div>
     <!-- /.navbar-collapse --> 
@@ -43,52 +38,34 @@
   <!-- /.container-fluid --> 
 </nav>
 
-
 <!-- Contact Section -->
 <div id="contact" class="text-center">
   <div class="overlay">
     <div class="container">
       <div class="col-md-8 col-md-offset-2 section-title">
-        <p class="btn-default" style="font-size: 20px"><b>You Have Got A Driver</b></p>
+        <h2>Choose Your Destination</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante facilisis bibendum dolor apibus lornare diam commodo nibh.</p>
       </div>
       <div class="col-md-8 col-md-offset-2">
         <form name="sentMessage" id="contactForm" novalidate>
           <div class="row">
               <div class="form-group">
-                <label><a style="font-size: 15px">Driver's Name :</a></label>
-                <input type="text" name="driverName" id="driverName" oninput="checkdupphone()" class="form-control" required="readonly">
+                <input type="text" name="location" id="us2-address" class="form-control" placeholder="Current Location" required="">
                 <p class="help-block text-danger"></p>
               </div>
               <div class="form-group">
-                <label><a style="font-size: 15px">Driver's Phone Number :</a></label>
-                <input type="text" name="driverContact" class="form-control" required readonly >
-                <p class="help-block text-danger"></p>
-              </div>
-              <div class="form-group">
-                <label><a style="font-size: 15px">Car Type :</a></label>
-                <input type="text" name="carType" class="form-control" required readonly >
-                <p class="help-block text-danger"></p>
-              </div>
-              <div class="form-group">
-                <label><a style="font-size: 15px">Car Plat Number :</a></label>
-                <input type="text" name="carPlat" class="form-control" required readonly >
-                <p class="help-block text-danger"></p>
-              </div>
-              <div class="form-group">
-                <label><a style="font-size: 15px">Car Color :</a></label>
-                <input type="text" name="carColor" class="form-control" required readonly >
+                <input type="text" name="location" id="us2-address" class="form-control" placeholder="Enter Destination" required="">
                 <p class="help-block text-danger"></p>
               </div>
           </div>
+          
           <div id="success"></div>
-          <input type="submit" name="userLogin" class="btn btn-default" value="Ride Now">
+          <input type="submit" name="driverRegister" class="btn btn-default" value="Search Available Platform">
         </form>
       </div>
     </div>
   </div>
 </div>
-
-
 
 <div id="footer">
   <div class="container text-center">
@@ -97,6 +74,7 @@
     </div>
   </div>
 </div>
+
 <script type="text/javascript" src="js/jquery.1.11.1.js"></script> 
 <script type="text/javascript" src="js/bootstrap.js"></script> 
 <script type="text/javascript" src="js/SmoothScroll.js"></script> 
@@ -105,5 +83,6 @@
 <script type="text/javascript" src="js/jqBootstrapValidation.js"></script> 
 <script type="text/javascript" src="js/contact_me.js"></script> 
 <script type="text/javascript" src="js/main.js"></script>
+
 </body>
 </html>
