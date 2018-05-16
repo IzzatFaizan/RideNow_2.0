9063936e82fc181a7321ed976ae68770d8a2b643
IzzatFaizan <izzatfaiz1996@gmail.com>
2018-05-16 11:53:30 +0800

    l

--- availablePlatform.php
+++ availablePlatform.php
@@ -8,7 +8,7 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title> RideNow | Driver Login</title>
+<title> RideNow | Book Car</title>
 <meta name="description" content="">
 <meta name="author" content="">
 
